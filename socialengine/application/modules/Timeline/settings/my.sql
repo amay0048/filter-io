1f4a77e6c8a9a07701812cbfc8e4e20a34bac90d
Anthony May <anthonyjmay@gmail.com>
2014-02-05 20:34:28 +1100

    Added the timeline plugin
    This allows you to upload cover art

---
+++ socialengine/application/modules/Timeline/settings/my.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `engine4_core_modules` (`name`, `title`, `description`, `version`, `enabled`, `type`) VALUES
+('timeline', 'Timeline', 'Hire-Experts LLC module', '4.2.2p3', 1, 'extra');
